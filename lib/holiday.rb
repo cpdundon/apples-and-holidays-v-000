6677b34799b80f15b94cf7232120684ef36f6826
Chris Dundon <chris.dundon@gmail.com>
2018-04-04 03:35:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -98,7 +98,9 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |k, v|
-    
+    v.each do |k_, v_|
+      
+    end
   end
 end
 
