e1a8c9b559a995baddc0f67e9bc412d31d98a6fa
Chris Dundon <chris.dundon@gmail.com>
2018-04-04 03:27:13 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,6 +80,9 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do |k, v|
     k_to_s = k.to_s.gsub(/[_]/, " " )
+    if k == :new_years
+      
+    end
     k_to_s = k_to_s.split.collect { |e| e.capitalize}
     k_to_s = k_to_s.join(" ")
     
